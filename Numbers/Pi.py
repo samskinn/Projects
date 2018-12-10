80eb46596de443dfe26374695dec70201a559108
samskinn <44012153+samskinn@users.noreply.github.com>
2018-12-10 18:24:31 +0000

    Update Pi.py

--- Numbers/Pi.py
+++ Numbers/Pi.py
@@ -1,1 +1,21 @@
+# Find Pi to the nth digit
+""" Pi is already a present in Python
+Need to import the Math function to call
+"""
 
+import math
+
+def defining_pi(roundVal):
+  ori_pi = round(math.pi, roundVal)
+  pi = str(ori_pi)
+  return ori_pi;
+
+roundTo = raw_input("Enter how many decimal places of Pi you want: ")
+try:
+  roundint = int(roundTo)
+  if roundint > 50:
+    print "Integer selected is too large"
+  else:
+    print defining_pi(roundint)
+  except:
+  print "You did not enter an integer"
