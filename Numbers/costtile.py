fc3d8b719ff1ae1a9597cc5ec73b11ccb1d91e49
samskinn <44012153+samskinn@users.noreply.github.com>
2018-12-11 12:26:34 +0000

    Update costtile.py

--- Numbers/costtile.py
+++ Numbers/costtile.py
@@ -1,1 +1,15 @@
+# Cost of tile to cover a floor plan of width and height
 
+def width(m):
+  m = int(input("Enter the width of the tile: "))
+  return m
+        
+def height(n):
+  n = int(input("Enter the height of the tile: "))
+  return n
+        
+def cost():
+  area = m * n
+  print "The area of the floor plan is " + area + "cm squared"
+  price = int(input("Enter the cost for the floor plan: "))
+                
