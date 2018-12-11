492ccdf21c12cf3eb9b9a5932c35b9d8676e473c
samskinn <44012153+samskinn@users.noreply.github.com>
2018-12-11 01:11:11 +0000

    Update primefactors.py

--- Numbers/primefactors.py
+++ Numbers/primefactors.py
@@ -1,1 +1,19 @@
+# finding out the prime factors of number inputed by user.
 
+import math
+
+n = int(input("Enter any integer to find its prime factors: "))
+
+print("Factors are:")
+i=1
+while(i<=n):
+    k=0
+    if(n%i==0):
+        j=1
+        while(j<=i):
+            if(i%j==0):
+                k=k+1
+            j=j+1
+        if(k==2):
+            print(i)
+    i=i+1  
