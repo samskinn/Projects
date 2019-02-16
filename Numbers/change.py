c4567b3a0b1ca5be12710a88db35a8f172769e6a
samskinn <44012153+samskinn@users.noreply.github.com>
2019-02-16 14:48:16 +0000

    Create change.py

---
+++ Numbers/change.py
@@ -0,0 +1,9 @@
+"""
+User enters cost, amount of money given. Calculate change in quarters, dimes, nickels, pennies
+"""
+
+
+cost = float(raw_input("What is the cost of the product?"))
+tender = float(raw_input("How much money are you giving to buy the product?"))
+
+
