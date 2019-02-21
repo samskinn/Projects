03ac29a9ae85576d7203c89980c1537532854532
samskinn <44012153+samskinn@users.noreply.github.com>
2019-02-21 16:10:51 +0000

    Update calculator.py

--- Numbers/calculator.py
+++ Numbers/calculator.py
@@ -1,1 +1,58 @@
+"""
+create a calculator
+function for adding, subtracting, dividing, and multiplying. 
+"""
+import math
 
+def add(num1, num2):
+  num1 = int(input(" "))
+  num2 = int(input(" "))
+  result = num1 + num2
+  print result
+  
+def sub(num1, num2):
+  num1 = int(input(" "))
+  num2 = int(input(" "))
+  result = num1 - num2
+  print result
+  
+
+
+def div(num1, num2):
+  num1 = int(input(" "))
+  num2 = int(input(" "))
+  result = num1 / num2
+  print result
+  
+
+
+def mul(num1, num2):
+  num1 = int(input(" "))
+  num2 = int(input(" "))
+  result = num1 * num2
+  print result 
+  
+  
+
+def sqr(num1):
+  num1 = int(input(" "))
+  result = num1 ** 2
+  print result
+  
+ 
+
+def prc(num1):
+  num1 = int(input(" "))
+  result = num1 / 100
+  print result
+  
+
+
+def log(num1):
+  num1 = int(input(" "))
+  result = math.log10(num1)
+  print result
+  
+ 
+
+  
