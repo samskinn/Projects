8c9c1ad3a778e589af3eb87ac7ff6653d1996b4a
samskinn <44012153+samskinn@users.noreply.github.com>
2019-02-21 15:10:19 +0000

    Update bidec.py

--- Numbers/bidec.py
+++ Numbers/bidec.py
@@ -1,1 +1,33 @@
+#Create a converter that changes binary numbers to decimals and vice versa 
 
+def b_to_d(number):
+  var = str(number)[::-1]
+  counter = 0
+  decimal = 0
+  while counter < len(var):
+    if int(var[counter]) == 1:
+      decimal += 2**counter
+      counter += 1
+  return decimal
+
+def d_to_b(number):
+  binary = ''
+  while number > 0:
+    binary += str(number % 2)
+    number = number / 2
+  return binary[::-1]
+
+print 'Enter 0 for binary to decimal conversion'
+print 'Enter 1 for decimal to binary conversion'
+
+choice = raw_input("Enter the conversion type: ")
+if choice == 0:
+  number = raw_input("Enter the number: ")
+  print "The decimal of %s is %s" % (number, b_to_d(number))
+  
+elif choice == 1:
+  number = raw_input("Enter the number: ")
+  print "The binary of %s is %s" 5 (number, d_to_b(number)) 
+  
+else:
+  print "Invalid choice"
