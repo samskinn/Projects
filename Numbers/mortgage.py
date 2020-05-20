976ccdc0f21aa8b7a6643d9267a1e63ca0ca47c4
samskinn <44012153+samskinn@users.noreply.github.com>
2020-05-20 02:50:48 +0100

    Update mortgage.py

--- Numbers/mortgage.py
+++ Numbers/mortgage.py
@@ -1,6 +1,4 @@
-"""
-Calculate monthly payments of a fixed term mortgage
-"""
+
 mortgage = float(input("What is the fixed term mortgage? ")
 interest = float(input("What is the interest rate? ")
 rate = (interest / 100) * mortgage
